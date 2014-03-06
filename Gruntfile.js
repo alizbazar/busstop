48949135f038d3d351e4b1c3f38c4295ae024196
Albert Nazander <alizbazar@alizweb.com>
2014-03-06 20:03:14 +0200

    Add Grunt tasks for uploading and generating cache manifest

---
+++ Gruntfile.js
@@ -0,0 +1,132 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    privateConfig: grunt.file.readJSON('private-config.json'),
+
+    manifest: {
+      generate: {
+        options: {
+          basePath: 'public/',
+          cache: [
+            // LIST HERE ALL INTERNAL EXPLICIT DEPENDENCIES
+            'icons/star-icon-36.png',
+            'icons/star-icon-empty-36.png',
+            // LIST HERE ALL EXTERNAL DEPENDENCIES
+            // TODO: this could probably be done automatically with using require.js
+            '//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css',
+            '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js',
+            '//cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.13.3/jquery.tablesorter.min.js',
+            '//cdnjs.cloudflare.com/ajax/libs/jquery-countdown/1.6.3/jquery.countdown.min.js'
+          ],
+          network: ['*'],
+          // fallback: ['/ /offline.html'],
+          // exclude: ['js/jquery.min.js'],
+          // preferOnline: true,
+          verbose: true,
+          timestamp: true,
+          // hash: true,
+          // master: ['index.html'] 
+        },
+        src: [
+          'index.html'
+        ],
+        dest: 'public/manifest.appcache'
+      }
+    },
+
+    aws_s3: {
+      options: {
+        accessKeyId: '<%= privateConfig.awsKey %>', // Use the variables
+        secretAccessKey: '<%= privateConfig.awsSecret %>', // You can also use env variables
+        region: '<%= privateConfig.awsRegion %>',
+        access: 'public-read',
+        uploadConcurrency: 5, // 5 simultaneous uploads
+        downloadConcurrency: 5 // 5 simultaneous downloads
+      },
+
+      production: {
+        options: {
+          bucket: '<%= privateConfig.awsBucket %>',
+          differential: true // Only uploads the files that have changed
+        },
+        files: [
+          {expand: true, cwd: 'public/', src: ['**', '!manifest.appcache'], dest: ''},
+          {expand: true, cwd: 'public/', src: ['manifest.appcache'], dest: '', params: {"CacheControl": "no-cache"} }
+        ]
+      }
+    },
+    // concat: {
+    //   options: {
+    //     banner: '<%= banner %>',
+    //     stripBanners: true
+    //   },
+    //   dist: {
+    //     src: ['lib/<%= pkg.name %>.js'],
+    //     dest: 'dist/<%= pkg.name %>.js'
+    //   }
+    // },
+    // uglify: {
+    //   options: {
+    //     banner: '<%= banner %>'
+    //   },
+    //   dist: {
+    //     src: '<%= concat.dist.dest %>',
+    //     dest: 'dist/<%= pkg.name %>.min.js'
+    //   }
+    // },
+    // jshint: {
+    //   options: {
+    //     curly: true,
+    //     eqeqeq: true,
+    //     immed: true,
+    //     latedef: true,
+    //     newcap: true,
+    //     noarg: true,
+    //     sub: true,
+    //     undef: true,
+    //     unused: true,
+    //     boss: true,
+    //     eqnull: true,
+    //     browser: true,
+    //     globals: {}
+    //   },
+    //   gruntfile: {
+    //     src: 'Gruntfile.js'
+    //   },
+    //   lib_test: {
+    //     src: ['lib/**/*.js', 'test/**/*.js']
+    //   }
+    // },
+    // qunit: {
+    //   files: ['test/**/*.html']
+    // },
+    // watch: {
+    //   gruntfile: {
+    //     files: '<%= jshint.gruntfile.src %>',
+    //     tasks: ['jshint:gruntfile']
+    //   },
+    //   lib_test: {
+    //     files: '<%= jshint.lib_test.src %>',
+    //     tasks: ['jshint:lib_test', 'qunit']
+    //   }
+    // }
+  });
+
+  // These plugins provide necessary tasks.
+  // grunt.loadNpmTasks('grunt-contrib-concat');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-qunit');
+  // grunt.loadNpmTasks('grunt-contrib-jshint');
+  // grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-aws-s3');
+  grunt.loadNpmTasks('grunt-manifest');
+
+  // Default task.
+  // grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+
+  grunt.registerTask('upload', ['manifest', 'aws_s3']);
+};
--- README.md
+++ README.md
@@ -6,3 +6,14 @@ HSL Bus Stop app that can be used to follow busses approaching the stop. Clickin
 ## Contributing
 
 If you would like to contribute for the project in any way, please reveal yourself! :) Would be great to create a nicer design for the app, as well as perhaps a functionality for selecting a stop somehow. Also other ideas are welcome!
+
+
+## Structure
+
+Currently public-folder is published to S3. In the future the idea is to develop a build process, which would minify and concatene files creating a separate release-ready directory.
+
+Backend is currently relying on PHP, however in the future the idea would be to use Node.js for backend operations.
+
+Structure of the frontend code is quite messy and the idea is to structure it with backbone and possible use some mobile-optimized UI framework, such as Chocolate Chip UI. Another possibility could be to implement this using App Gyver in order to package it for distribution in different app stores.
+
+Grunt task is to be created for publishing changes to S3.
